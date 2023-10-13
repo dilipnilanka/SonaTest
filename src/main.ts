de071678a65cc95db6db103046375e7802d9ff95
dilip-nilanka <dilip.nilanka@sa.gov.au>
2023-10-13 15:49:16 +1030

    test code

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { doAuth } from 'auth';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
@@ -7,6 +8,10 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
+});
+
+app.post('/auth', (req, res) => {
+  res.send(doAuth());
 });
 
 app.listen(port, host, () => {
